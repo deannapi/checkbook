95e21654b59abde449f1c1e25c041ef72e0b0515
deannapi <deannapi.mart@gmail.com>
2021-05-18 10:17:04 -0500

    committing before i delete methods

--- client/src/components/transactionForm.js
+++ client/src/components/transactionForm.js
@@ -2,14 +2,25 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import { ADD_TRANSACTION } from "../utils/mutations";
 import { QUERY_ME_BASIC, QUERY_TRANSACTIONS } from "../utils/queries";
-var $ = require("jquery");
+// var $ = require("jquery");
 
 export default function TransactionForm() {
-  const [transactionName, transactionAmount, username] = useState("");
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     transactionName: "",
+  //     transactionAmount: "",
+  //     items: [],
+  //   };
+  // }
+
+  const [transactionName, transactionAmount] = useState("");
   const [formState, setFormState] = useState({
     transactionName: "",
     transactionAmount: "",
   });
+
   const [addTransaction] = useMutation(ADD_TRANSACTION, {
     update(cache, { data: { addTransaction } }) {
       try {
@@ -38,8 +49,11 @@ export default function TransactionForm() {
     },
   });
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+  const handleChange = (e) => {
+    let input = e.target;
+    let name = e.target.name;
+    let value = input.value;
+
     setFormState({
       ...formState,
       [name]: value,
@@ -49,122 +63,81 @@ export default function TransactionForm() {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
+    let items = [...formState.items];
+
+    items.push({
+      transactionName: this.formState.transactionName,
+      transactionAmount: this.formState.transactionAmount,
+    });
+
+    this.setState({
+      items,
+      transactionName: "",
+      transactionAmount: "",
+    });
+
     try {
       // add transaction to database
       await addTransaction({
-        variables: { transactionName, transactionAmount, username },
+        variables: { transactionName, transactionAmount },
       });
-
-      //   clear form
-      transactionName("");
-      transactionAmount("");
     } catch (e) {
       console.error(e);
     }
   };
 
-  // function addingTransactions() {
-  $(() => {
-    $("#add-btn").on("click", function () {
-      var transName = $("#t-name").val();
-      var transAmount = $("#t-amount").val();
-      
-      var markup =
-        "<tr> <td>" +
-        transName +
-        "</td><td>" +
-        transAmount +
-        "</td><td><input type='checkbox' name='record'></td></tr>";
+    return (
+      <>
+        <div className="checkbook">
+          <div className="total">
+            Your total is: $<p id="total">0</p>
+          </div>
 
-      $("table tbody").append(markup);
+          <form
+            onSubmit={handleFormSubmit}
+            handleChange={handleChange}
+          >
+            <input
+              type="text"
+              className="form-control"
+              id="t-name"
+              name="transactionName"
+              placeholder="Name of transaction"
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              step=".01"
+              name="transactionAmount"
+              min="0"
+              className="form-control"
+              id="t-amount"
+              placeholder="Transaction amount"
+              onChange={handleChange}
+            />
+            <button id="add-btn" className="btn">
+              <i className="fas fa-plus"></i> Add Funds
+            </button>
+            <button id="sub-btn" className="btn">
+              <i className="fas fa-minus"></i> Subtract Funds
+            </button>
+            <p className="error-text" role="alert"></p>
+          </form>
 
-      // update the current balance
-      $("#total").val(transAmount);
-    });
-  });
-
-  // function subtractTransactions() {
-  $(() => {
-    $("#sub-btn").on("click", function () {
-      var transName2 = $("#t-name").val();
-      var transAmount2 = $("#t-amount").val() * -1;
-      $("#total").val(transAmount2);
-
-      var markup2 =
-        "<tr> <td>" +
-        transName2 +
-        "</td><td>" +
-        transAmount2 +
-        "</td><td><input type='checkbox' name='record'></td></tr>";
-
-      $("table tbody").append(markup2);
-    });
-  });
-  // )
-
-  $(() => {
-    $("#del-btn").on("click", function () {
-      $("table tbody")
-        .find('input[name="record"]')
-        .each(function () {
-          if ($(this).is(":checked")) {
-            $(this).parents("tr").remove();
-          }
-        });
-    });
-  });
-
-  return (
-    <>
-      <div className="checkbook">
-        <div className="total">
-          Your total is: $<span id="total"></span>
+          <div className="transactions">
+            <table id="transTable">
+              <thead>
+                <tr>
+                  {/* <th>Date</th> */}
+                  <th>Transaction</th>
+                  <th>Amount</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody id="tbody"></tbody>
+            </table>
+          </div>
         </div>
-
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            className="form-control"
-            id="t-name"
-            name="transactionName"
-            placeholder="Name of transaction"
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            step=".01"
-            name="transactionAmount"
-            min="0"
-            className="form-control"
-            id="t-amount"
-            placeholder="Transaction amount"
-            onChange={handleChange}
-          />
-          <button id="add-btn" className="btn">
-            <i className="fas fa-plus"></i> Add Funds
-          </button>
-          <button id="sub-btn" className="btn">
-            <i className="fas fa-minus"></i> Subtract Funds
-          </button>
-          <p className="error-text" role="alert"></p>
-        </form>
-
-        <div className="transactions">
-          <table id="transTable">
-            <thead>
-              <tr>
-                <th>Transaction</th>
-                <th>Amount</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody id="tbody"></tbody>
-          </table>
-          <button type="button" id="del-btn">
-            Delete Transaction
-          </button>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
